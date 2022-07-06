1bf01f61a7acbabea40fec021ba828e6a2d71725
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-07-06 03:28:05 +0000

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Polygon{
+  constructor(array){
+    this.side = array;
+  }
+
+  get countSides(){
+    return this.side.length;
+  }
+
+  get perimeter(){
+    return this.side.reduce((arr1, arr2)=> arr1 + arr2);
+  }
+}
+
+
+class Triangle extends Polygon{
+  get isValid(){
+    if(((this.side[0])+(this.side[1])) > this.side[2]){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
+class Square extends Polygon{
+  get isValid(){
+    if(this.side[0]===this.side[1]===this.side[2]===this.side[2]){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  get area(){
+    return this.side[0]*this.side[0];
+  }
+}
+
